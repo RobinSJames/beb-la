4cb36875dabfe833268a67663e9a8665a16c162c
Stuart <robinjamesauthor@gmail.com>
2019-11-25 23:24:58 +0200

    initialised app and created components

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <b-nav tabs fill>
+      <b-nav-item disabled>Another Link</b-nav-item>
+      <b-nav-item disabled>Counter: {{ numCorrect }}/{{ numTotal }}</b-nav-item>
+    </b-nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  props: ["numCorrect", "numTotal"]
+};
+</script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,89 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <Header :numCorrect="numCorrect" :numTotal="numTotal" />
+    <div class="div">Timer: {{ countDown }}</div>
+    <div v-if="countDown === 0" class="cons">Times up</div>
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col sm="10" offset="1">
+          <QuestionBox
+            v-if="questions.length"
+            :currentQuestion="questions[index]"
+            :next="next"
+            :increment="increment"
+          />
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
+
+<script>
+import Header from "../components/Header.vue";
+import QuestionBox from "../components/QuestionBox";
+import { setTimeout } from "timers";
+
+export default {
+  name: "app",
+  components: {
+    QuestionBox,
+    Header
+  },
+  data() {
+    return {
+      questions: [],
+      index: 0,
+      numCorrect: 0,
+      numTotal: 0,
+      countDown: null
+    };
+  },
+  methods: {
+    state() {
+      if (this.countDown === 0) {
+        // disable eslint no-console
+        console.log("zero");
+      }
+    },
+    reset() {
+      this.countDown = null;
+      this.countDown = 10;
+    },
+    next() {
+      this.index++;
+      this.reset();
+      // this.countDown = 10
+    },
+    increment(isCorrect) {
+      if (isCorrect) {
+        this.numCorrect++;
+      }
+      this.numTotal++;
+    },
+    countDownTimer() {
+      if (this.countDown > 0) {
+        setTimeout(() => {
+          this.countDown -= 1;
+          this.countDownTimer();
+        }, 1000);
+      }
+    }
+  },
+  mounted: function() {
+    fetch(
+      "https://opentdb.com/api.php?amount=20&category=22&difficulty=easy&type=multiple",
+      {
+        method: "get"
+      }
+    )
+      .then(response => {
+        return response.json();
+      })
+      .then(jsonData => {
+        this.questions = jsonData.results;
+        this.reset();
+        this.countDownTimer();
+      });
+  }
+};
+</script>
