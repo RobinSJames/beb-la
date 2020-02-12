ccad90323a320917b8e1849f1dd2c7ada5d9e967
Stuart <robinjamesauthor@gmail.com>
2020-02-12 18:12:44 +0200

    Added lottiefiles to project
    Lottiefiles added to allow for simple implementation of animation.

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,12 +1,15 @@
 <template>
   <div>
+    <AnimationPlay src="drink_shifting" class="animation" />
     <b-button pill @click="nextPage">SELECT GAME OPTIONS</b-button>
   </div>
 </template>
 
 <script>
+import AnimationPlay from "./AnimationPlay";
 export default {
   name: "LandingPage",
+  components: { AnimationPlay },
   methods: {
     nextPage() {
       this.$emit("clicked");
@@ -15,4 +18,9 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.animation {
+  height: 200px;
+  width: 200px;
+}
+</style>
